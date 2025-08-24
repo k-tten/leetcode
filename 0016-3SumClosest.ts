5a147e10b15c243ade3f57127ecb0afcaab43f82
ten <cursors.owo.uwu@gmail.com>
2025-08-23 22:56:05 -0500

    Create 0016-3SumClosest.ts

---
+++ 0016-3SumClosest.ts
@@ -0,0 +1,25 @@
+function threeSumClosest(nums: number[], target: number): number {
+    nums.sort((a, b) => a - b);
+    
+    let closest = Infinity;
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1, right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (Math.abs(target - sum) < Math.abs(target - closest)) {
+                closest = sum;
+                continue;
+            }
+
+            if (sum < target) left++;
+            else right--;
+        }
+    }
+
+    return closest;
+}
